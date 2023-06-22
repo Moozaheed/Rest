1572c0857ae751f6d3166311660ee16a4f185323
Moozaheed <57921369+Moozaheed@users.noreply.github.com>
2023-06-23 01:23:20 +0600

    first commit

---
+++ restaurant_website.sql
@@ -0,0 +1,429 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 17, 2023 at 03:55 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restaurant_website`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `client_id` int(5) NOT NULL,
+  `client_name` varchar(50) NOT NULL,
+  `client_phone` varchar(20) NOT NULL,
+  `client_email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`client_id`, `client_name`, `client_phone`, `client_email`) VALUES
+(9, 'Idriss Jairi', '0634308303', 'qsdqsdq@gmail.com'),
+(10, 'Khalid Lee', '0638383933', 'khalid.lee@gmail.com'),
+(11, 'Keltoum', '06242556272', 'keltoum.ar@gmail.com'),
+(13, 'Shukhrat Nayimov', '030303030202', 'shukh.nayi@gmail.com'),
+(14, 'Khalid Essaidani', '030303030', 'khalid.essaidani@yahoo.fr'),
+(15, '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_gallery`
+--
+
+CREATE TABLE `image_gallery` (
+  `image_id` int(2) NOT NULL,
+  `image_name` varchar(30) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `image_gallery`
+--
+
+INSERT INTO `image_gallery` (`image_id`, `image_name`, `image`) VALUES
+(1, 'moroccan tajine', '58146_Moroccan Chicken Tagine.jpeg'),
+(2, 'Italian Pasta', 'img_1.jpg'),
+(3, 'Cook', 'img_2.jpg'),
+(4, 'Pizza', 'img_3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `in_order`
+--
+
+CREATE TABLE `in_order` (
+  `id` int(5) NOT NULL,
+  `order_id` int(5) NOT NULL,
+  `menu_id` int(5) NOT NULL,
+  `quantity` int(3) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `in_order`
+--
+
+INSERT INTO `in_order` (`id`, `order_id`, `menu_id`, `quantity`) VALUES
+(3, 7, 13, 1),
+(4, 7, 11, 1),
+(5, 8, 1, 1),
+(6, 8, 2, 1),
+(7, 9, 11, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menus`
+--
+
+CREATE TABLE `menus` (
+  `menu_id` int(2) NOT NULL,
+  `menu_name` varchar(20) NOT NULL,
+  `menu_description` varchar(255) NOT NULL,
+  `menu_price` decimal(4,2) NOT NULL,
+  `menu_image` varchar(255) NOT NULL,
+  `category_id` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `menus`
+--
+
+INSERT INTO `menus` (`menu_id`, `menu_name`, `menu_description`, `menu_price`, `menu_image`, `category_id`) VALUES
+(1, 'Xincent', 'Classic Marinara Sauce', '14.00', '86749_b3.jpg', 1),
+(2, 'Margherita', 'Authentic old-world pepperoni', '15.00', '95939_b2.jpg', 1),
+(4, 'Bostrengo', 'Originating from the region of Romagna', '5.50', 'summer-dessert-sweet-ice-cream.jpg', 2),
+(9, 'Margherita', 'Classic marinara sauce, authentic old-world pepperoni.', '24.00', 'pizza.jpeg', 5),
+(10, 'Diablo', 'Classic marinara sauce, authentic old-world pepperoni.', '10.00', 'pizza_plate.jpeg', 5),
+(11, 'Tajine', 'Moroccan Tagine in China', '20.00', '47660_b1.jpg', 1),
+(13, 'COUSCOUS', 'COUSCOUS BIL ADASS', '99.99', '68526_57738_w1024h768c1cx256cy192.jpg', 4),
+(15, 'TEST NAME', 'TETTE TSTSTSTS', '20.00', '61131_1200px-Flag_of_Morocco.svg.png', 5),
+(16, 'Couscous', 'Moroccan Couscous', '20.00', '93684_images.jpg', 1),
+(17, 'Aflatoon', 'Flavored with cardamom, saffron, and rose water', '10.00', '63278_ds1.jpg', 2),
+(18, 'Kulfi', 'Traditional Indian ice cream', '7.50', '89595_ds4.jpg', 2),
+(19, 'Cake', 'Flavored with vanilla extract, cocoa powder, fruits, and spices', '20.40', '722_ds2.jpg', 2),
+(20, 'Mojito', 'Refreshing and citrusy cocktail with a hint of mint', '5.00', '27309_d4.jpg', 3),
+(21, 'Vegetable Juices', 'Made from carrot, beetroot, or tomato', '13.00', '68112_d3.jpg', 3),
+(23, 'Smoothies', 'Fruits, milk, and permissible sweeteners', '10.00', '80721_d5.jpg', 3),
+(24, 'Mocktails', 'Without any alcohol-based ingredients', '24.40', '35690_d2.jpg', 3),
+(25, 'Spaghetti', 'Long, thin pasta strands that are versatile', '99.99', '2367_p3.jpg', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu_categories`
+--
+
+CREATE TABLE `menu_categories` (
+  `category_id` int(2) NOT NULL,
+  `category_name` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `menu_categories`
+--
+
+INSERT INTO `menu_categories` (`category_id`, `category_name`) VALUES
+(1, 'burgers'),
+(2, 'desserts'),
+(3, 'drinks'),
+(4, 'pasta'),
+(5, 'pizzas'),
+(6, 'salads');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `placed_orders`
+--
+
+CREATE TABLE `placed_orders` (
+  `order_id` int(5) NOT NULL,
+  `order_time` datetime NOT NULL,
+  `client_id` int(5) NOT NULL,
+  `delivery_address` varchar(255) NOT NULL,
+  `delivered` tinyint(1) NOT NULL DEFAULT 0,
+  `canceled` tinyint(1) NOT NULL DEFAULT 0,
+  `cancellation_reason` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `placed_orders`
+--
+
+INSERT INTO `placed_orders` (`order_id`, `order_time`, `client_id`, `delivery_address`, `delivered`, `canceled`, `cancellation_reason`) VALUES
+(7, '2020-06-22 12:01:00', 9, 'Bloc A Nr 80000 Hay El Houda Agadir', 0, 1, 'Sorry! I changed my mind!'),
+(8, '2020-06-23 06:07:00', 10, 'Chengdu, China', 0, 0, ''),
+(9, '2020-06-24 16:40:00', 11, 'Hay El Houda Agadir', 1, 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `reservation_id` int(5) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `client_id` int(5) NOT NULL,
+  `selected_time` datetime NOT NULL,
+  `nbr_guests` int(2) NOT NULL,
+  `table_id` int(3) NOT NULL,
+  `liberated` tinyint(1) NOT NULL DEFAULT 0,
+  `canceled` tinyint(1) NOT NULL DEFAULT 0,
+  `cancellation_reason` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`reservation_id`, `date_created`, `client_id`, `selected_time`, `nbr_guests`, `table_id`, `liberated`, `canceled`, `cancellation_reason`) VALUES
+(1, '2020-07-18 09:07:00', 13, '2020-07-30 09:07:00', 0, 1, 0, 0, NULL),
+(2, '2020-07-18 09:11:00', 14, '2020-07-29 13:00:00', 4, 1, 0, 0, NULL),
+(3, '2023-06-16 22:53:00', 15, '2023-06-17 22:53:00', 1, 1, 0, 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tables`
+--
+
+CREATE TABLE `tables` (
+  `table_id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tables`
+--
+
+INSERT INTO `tables` (`table_id`) VALUES
+(1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(2) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `full_name` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `full_name`, `password`) VALUES
+(1, 'tusha', 'tusha@gmail.com', 'Tusha', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `website_settings`
+--
+
+CREATE TABLE `website_settings` (
+  `option_id` int(5) NOT NULL,
+  `option_name` varchar(255) NOT NULL,
+  `option_value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `website_settings`
+--
+
+INSERT INTO `website_settings` (`option_id`, `option_name`, `option_value`) VALUES
+(1, 'restaurant_name', 'VINCENT PIZZA'),
+(2, 'restaurant_email', 'vincent.pizza@gmail.com'),
+(3, 'admin_email', 'tusha@gmail.com'),
+(4, 'restaurant_phonenumber', '0123456789'),
+(5, 'restaurant_address', 'Wasa, Chittagong, Bangladesh');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `image_gallery`
+--
+ALTER TABLE `image_gallery`
+  ADD PRIMARY KEY (`image_id`);
+
+--
+-- Indexes for table `in_order`
+--
+ALTER TABLE `in_order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_menu` (`menu_id`),
+  ADD KEY `fk_order` (`order_id`);
+
+--
+-- Indexes for table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`menu_id`),
+  ADD KEY `FK_menu_category_id` (`category_id`);
+
+--
+-- Indexes for table `menu_categories`
+--
+ALTER TABLE `menu_categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `placed_orders`
+--
+ALTER TABLE `placed_orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `fk_client` (`client_id`);
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`reservation_id`);
+
+--
+-- Indexes for table `tables`
+--
+ALTER TABLE `tables`
+  ADD PRIMARY KEY (`table_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `website_settings`
+--
+ALTER TABLE `website_settings`
+  ADD PRIMARY KEY (`option_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `client_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `image_gallery`
+--
+ALTER TABLE `image_gallery`
+  MODIFY `image_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `in_order`
+--
+ALTER TABLE `in_order`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `menu_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `menu_categories`
+--
+ALTER TABLE `menu_categories`
+  MODIFY `category_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `placed_orders`
+--
+ALTER TABLE `placed_orders`
+  MODIFY `order_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `reservation_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tables`
+--
+ALTER TABLE `tables`
+  MODIFY `table_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `website_settings`
+--
+ALTER TABLE `website_settings`
+  MODIFY `option_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `in_order`
+--
+ALTER TABLE `in_order`
+  ADD CONSTRAINT `fk_menu` FOREIGN KEY (`menu_id`) REFERENCES `menus` (`menu_id`),
+  ADD CONSTRAINT `fk_order` FOREIGN KEY (`order_id`) REFERENCES `placed_orders` (`order_id`);
+
+--
+-- Constraints for table `menus`
+--
+ALTER TABLE `menus`
+  ADD CONSTRAINT `FK_menu_category_id` FOREIGN KEY (`category_id`) REFERENCES `menu_categories` (`category_id`);
+
+--
+-- Constraints for table `placed_orders`
+--
+ALTER TABLE `placed_orders`
+  ADD CONSTRAINT `fk_client` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
